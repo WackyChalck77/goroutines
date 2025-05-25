58d933096396763d3aff7a438c35f6623b2337a9
WackyChalck77 <vsevolod.evtukhov@gmail.com>
2025-05-26 02:13:27 +0300

    task 6 added

--- 05/main.go
+++ 05/main.go
@@ -5,12 +5,12 @@ import (
 	"sync/atomic"
 )
 
-var counter uint32 //положительный счетчик
+var counter uint32 //положительный счетчик __
 
 func main() {
 	for i := 0; i < 50; i++ { //50 горутин
 		go func() {
-			for c := 0; c < 100; c++ { //выполняет 100 раз
+			for c := 0; c < 100; c++ { //выполняет 100 раз__
 				atomic.AddUint32(&counter, 1) //атомарное прибавление единицы
 			}
 		}()
